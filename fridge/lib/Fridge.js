b35d4a0ebd2430b74fa49cdc1de09506586d4005
henri <henri.ruetzler@gmx.de>
2024-03-16 18:27:50 +0100

    MQTT bei der Kühlschrankseite hinzugefügt

--- fridge/lib/Fridge.js
+++ fridge/lib/Fridge.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const fs = require('fs');
 const basicAuth = require('express-basic-auth');
+const mqtt = require('mqtt');
 var daten = {"verriegelt":false,"zeiten":[]};
 var verbindungen = new Array();
 
+const broker = 'mqtts://sruetzler.de:8883';
+const clientId = 'HenriFridge';
+const username = 'henri';
+const password = 'Br@hn*6^ZD*kaK';
+
+const options = {
+  clientId,
+  username,
+  password,
+  cleanSession: true,
+  rejectUnauthorized: false,
+  ca: null // oder []
+};
+
+const client = mqtt.connect(broker, options);
 
 class Fridge{
   constructor(){
@@ -19,11 +35,7 @@ class Fridge{
       // expressApp.settings('views', './fridge/lib/views');
       expressApp.set('view engine', 'ejs');
 
-      expressApp.use((req, res, next) =>{
-        if (req.url.startsWith("/fridge")) {
-          next();
-        }
-      });
+
 
       expressApp.set('views', './fridge/views');
 
@@ -74,8 +86,11 @@ class Fridge{
         res.status(200).send();
       });
 
-      expressApp.use((req, res) =>{
-        res.status(404).render('404', {title: '404'});
+      expressApp.use((req, res, next) =>{
+        if (req.url.startsWith("/fridge")) {
+          res.status(404).render('404', {title: '404'});
+        }
+        next();
       });
     }
 
@@ -84,15 +99,7 @@ class Fridge{
       fs.writeFile('./fridge/data/daten.json', json , () =>{
           // console.log('file has written')
       });
-      verbindungen.forEach(function (list) {
-        try{
-          const socket = list[1];
-          // console.log(socket);
-          socket.emit('data', JSON.stringify(daten));
-        }catch(error){
-          console.error(error);
-        }
-      })
+      client.publish('henri/fridge', JSON.stringify(daten));
     
     }
 
@@ -110,50 +117,7 @@ class Fridge{
       });
     }
 
-    wsUebergeben(_io){
-      const io = _io;
-      io.of("/fridge" ).on('connection', (socket) => {
-        console.log('a user connected');
-        var id = null;
-        
-        
-        if (verbindungen.length != 0) {
-          id = verbindungen[verbindungen.length -1][0] +1;
-        }else{
-          id = 0;
-        }
-        verbindungen.push([id, socket]);
-        // verbindungen[0][1].emit('data', JSON.stringify(daten));
-        verbindungen[verbindungen.length -1][1].emit('data', JSON.stringify(daten));
-        // socket.emit('data', JSON.stringify(daten));
-        // console.log(verbindungen);
-        
-
-        socket.on('disconnect', () => {
-          console.log('user disconnected');
-          for (let i = 0; i < verbindungen.length; i++) {
-            const stelle = verbindungen[i][0];
-            if (id == stelle) {
-              verbindungen.splice(i, 1);
-              break;
-            }
-            
-          }
-          // console.log(verbindungen);
-        });
-
-
-
-
-
-        // socket.on("hugo", (data, cb)=>{
-        //   console.log(data)
-        //   socket.emit("emil",data, (response)=>{
-
-        //   });
-        // })
-      });
-    }
+   
 
     basicAuthMiddleware = basicAuth({
       users: { 'admin': 'admin' }, // Hier deine Benutzername-Passwort-Kombinationen eintragen
